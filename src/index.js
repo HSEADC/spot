95bbd74a794250ac6def94cddd98db7922e83190
Zay <z.znaranzaya@gmail.com>
2024-10-21 23:45:07 +0300

    basics style, landing page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css'
-import '../src/fonts/TT-Prosto-Sans-Trial-Bold.otf'
-import '../src/fonts/TT-Prosto-Sans-Trial-Regular.otf'
+// import '../src/fonts/TT-Prosto-Sans-Trial-Bold.otf'
+// import '../src/fonts/TT-Prosto-Sans-Trial-Regular.otf'
 
 document.addEventListener('DOMContentLoaded', () => {
   const image = new Image()
