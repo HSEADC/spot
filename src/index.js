3cf448e738a2a86945c5431cb24906181ce66abd
Zay <z.znaranzaya@gmail.com>
2024-10-22 15:19:06 +0300

    adaptive

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './index.css'
-// import '../src/fonts/TT-Prosto-Sans-Trial-Bold.otf'
-// import '../src/fonts/TT-Prosto-Sans-Trial-Regular.otf'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const image = new Image()
-  image.src = jpg
-  document.querySelector('.images').appendChild(image)
-})
