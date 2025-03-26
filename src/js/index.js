051b6fcd812728dd01ba3a29c9da43235dd8d6c8
Zay <z.znaranzaya@gmail.com>
2025-03-27 00:31:34 +0300

    try nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  console.log(' Script loaded!')
   // When clicking .main_cover, add 'none' class to <nav>
   $('.main_cover').click(function () {
     $('nav').addClass('none')
