620735f86e0342b41fc145355b5315e9f4e5d28c
Zay <z.znaranzaya@gmail.com>
2025-03-27 00:06:56 +0300

    try nav

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,9 @@ $(document).ready(function () {
     $('nav').addClass('none')
   })
 })
+document.getElementById('toggleBtn').addEventListener('click', function () {
+  const navMenu = document.getElementById('navMenu')
+
+  // Toggle 'active' class
+  navMenu.classList.toggle('active')
+})
