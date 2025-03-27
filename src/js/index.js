3c1de33f6a4bf7fd166b9826e675948da0440efb
Zay <z.znaranzaya@gmail.com>
2025-03-27 16:13:34 +0300

    js

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,36 @@ $(document).ready(function () {
 
   // Toggle 'active' class on #navMenu when clicking #toggleBtn
 })
+document.addEventListener('DOMContentLoaded', () => {
+  initFilter()
+  initBurgerMenu()
+  filterAll()
+  filterByTag()
+})
+function initBurgerMenu() {
+  // Toggle the nav menu
+  $('#toggleBtn').click(function () {
+    $('#navMenu').toggleClass('active')
+  })
+
+  // Change the burger menu icon image on click
+  $('#toggleBtn').click(function () {
+    var img = $('#burgerIcon') // Select the image
+
+    // Check the current image source and toggle between two icons
+    if (
+      img.attr('src') ===
+      'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
+    ) {
+      img.attr(
+        'src',
+        'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerClose.svg'
+      )
+    } else {
+      img.attr(
+        'src',
+        'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
+      )
+    }
+  })
+}
