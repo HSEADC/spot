10a45a7a9dc94c26142dd729d3b86718793dd2d4
Zay <z.znaranzaya@gmail.com>
2025-05-20 14:57:27 +0300

    burger

--- src/js/menu.js
+++ src/js/menu.js
@@ -28,26 +28,53 @@ document.addEventListener('DOMContentLoaded', () => {
 //     }
 //   })
 // }
+
+/* van js*/
+// function initBurgerMenu() {
+//   // Toggle the nav menu
+//   const toggleButton = document.querySelector('#toggleBtn')
+
+//   toggleButton.addEventListener('click', () => {
+//     document.querySelector('#navMenu').classList.toggle('active')
+//   })
+
+//   toggleButton.addEventListener('click', () => {
+//     let img = document.querySelector('#burgerIcon')
+//     // Check the current image source and toggle between two icons
+//     if (
+//       img.src ===
+//       'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
+//     ) {
+//       img.src ==
+//         'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerClose.svg'
+//     } else {
+//       img.src ==
+//         'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
+//     }
+//   })
+// }
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', initBurgerMenu)
+} else {
+  initBurgerMenu()
+}
+
 function initBurgerMenu() {
-  // Toggle the nav menu
   const toggleButton = document.querySelector('#toggleBtn')
+  const navMenu = document.querySelector('#navMenu')
+  const img = document.querySelector('#burgerIcon')
 
   toggleButton.addEventListener('click', () => {
-    document.querySelector('#navMenu').classList.toggle('active')
-  })
+    navMenu.classList.toggle('active')
 
-  toggleButton.addEventListener('click', () => {
-    let img = document.querySelector('#burgerIcon')
-    // Check the current image source and toggle between two icons
-    if (
-      img.src ===
-      'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
-    ) {
-      img.src ==
-        'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerClose.svg'
+    const menuIcon = 'A_BurgerMenu.svg'
+    const closeIcon = 'A_BurgerClose.svg'
+
+    if (img.src.includes(menuIcon)) {
+      img.src = img.src.replace(menuIcon, closeIcon)
     } else {
-      img.src ==
-        'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
+      img.src = img.src.replace(closeIcon, menuIcon)
     }
   })
 }
