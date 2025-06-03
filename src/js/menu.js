136c5be678d1a53b7ea36a8d5bdaecb7184639db
Zay <z.znaranzaya@gmail.com>
2025-06-03 14:58:42 +0300

    tips2

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,33 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   initBurgerMenu()
 })
-// function initBurgerMenu() {
-//   // Toggle the nav menu
-//   $('#toggleBtn').click(function () {
-//     $('#navMenu').toggleClass('active')
-//   })
-
-//   // Change the burger menu icon image on click
-//   $('#toggleBtn').click(function () {
-//     var img = $('#burgerIcon') // Select the image
-
-//     // Check the current image source and toggle between two icons
-//     if (
-//       img.attr('src') ===
-//       'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
-//     ) {
-//       img.attr(
-//         'src',
-//         'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerClose.svg'
-//       )
-//     } else {
-//       img.attr(
-//         'src',
-//         'https://raw.githubusercontent.com/HSEADC/spot/refs/heads/main/src/images/components/02_Atoms/A_BurgerMenu.svg'
-//       )
-//     }
-//   })
-// }
 
 /* van js*/
 function initBurgerMenu() {
@@ -52,28 +25,3 @@ function initBurgerMenu() {
     }
   })
 }
-
-// if (document.readyState === 'loading') {
-//   document.addEventListener('DOMContentLoaded', initBurgerMenu)
-// } else {
-//   initBurgerMenu()
-// }
-
-// function initBurgerMenu() {
-//   const toggleButton = document.querySelector('#toggleBtn')
-//   const navMenu = document.querySelector('#navMenu')
-//   const img = document.querySelector('#burgerIcon')
-
-//   toggleButton.addEventListener('click', () => {
-//     navMenu.classList.toggle('active')
-
-//     const menuIcon = 'A_BurgerMenu.svg'
-//     const closeIcon = 'A_BurgerClose.svg'
-
-//     if (img.src.includes(menuIcon)) {
-//       img.src = img.src.replace(menuIcon, closeIcon)
-//     } else {
-//       img.src = img.src.replace(closeIcon, menuIcon)
-//     }
-//   })
-// }
